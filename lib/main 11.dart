c0b94005d3340cc069a8ecaf83a2f3a1e3c402ae
paneexiong970-maker <paneexiong970@gmail.com>
2025-11-14 22:46:31 +0700

    Fix app title in lesson 11 to reflect correct naming

--- lib/main 11.dart
+++ lib/main 11.dart
@@ -9,7 +9,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('my first app jukkajue' ),
+        title: Text('my first app lesson 11' ),
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
