b36694dbe971ead9ff75b5fb5de4a4a808c05daf
paneexiong970-maker <paneexiong970@gmail.com>
2025-11-17 21:00:19 +0700

    Refactor Home widget layout and update body structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,48 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home:Home(),
-));
+void main() => runApp(MaterialApp(home: Home()));
 
- 
 class Home extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return  Scaffold(
-    appBar:AppBar(
-      title: Text('my first app '),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: <Widget>[
-        Row(
-          children: <Widget>[
-            Text('hello'),
-            Text('world'),
-          ]
-        ),
-        Container(
-          padding: EdgeInsets.all(20.0),
-          color: Colors.cyan,
-          child: Text(
-            'one',
-            style: TextStyle(fontWeight: FontWeight.bold),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my first app '),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Row(
+        children: <Widget>[
+          Expanded(child: Image.asset('assets/A.png')),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.cyan,
+              child: Text('1'),   
+               ),
+            ), 
+          
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.amber,
+              child: Text('2'),
+              ),
           ),
-        ),
-        Container(
-          padding: EdgeInsets.all(30.0),
-          color: Colors.amber,
-          child: Text(
-            'two',
-            style: TextStyle(fontWeight: FontWeight.bold),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.pinkAccent,
+              child: Text('3'), 
+              ),
           ),
-        ),
-        Container(
-          padding: EdgeInsets.all(40.0),
-          color: Colors.pinkAccent,
-          child: Text(
-            'three',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-        ),
-      ],
-    ),
-    
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {  },
-      child: Text('click'),
-      backgroundColor: Colors.red[600],
-    ),
-  );
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
   }
 }
-
